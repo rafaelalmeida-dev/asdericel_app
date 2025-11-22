b19b05cc235219786451e4c58dfca4c259e9d344
rafael dev <rafaelalmeidadev@gmail.com>
2025-11-22 15:43:44 -0400

    ajustado pagina principal

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  get "club-history" => "externo#club_history", as: :club_history
   # Defines the root path route ("/")
   root "home#index"
+
 end
