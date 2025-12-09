1fcf320ac4a421ae3a25d86950ae0b61218e5743
rafael dev <rafaelalmeidadev@gmail.com>
2025-12-09 18:33:47 -0400

    ajuste no dropdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "bootstrap"
 
 // JS do Sneat (separado por pastas)
 import "./sneat/helpers"
