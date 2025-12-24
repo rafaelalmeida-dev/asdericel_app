66243d94a403fbd1ffaa8d5c1c2cc67a988c0779
daniel <danielrm2013@gmail.com>
2025-12-24 02:15:47 +0000

    assets

---
+++ public/assets/application-5a4ea79a96fea9abae248d5d87584f3499efe187576d29fcb117cfbf22e9310f.js
@@ -0,0 +1,7 @@
+// // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// JS do Sneat (separado por pastas)
+import "./sneat/helpers"
+import "./sneat/menu"
+import "./controllers/confirm_delete"
+;
