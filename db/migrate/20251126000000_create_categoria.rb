e0d46d3bdb68ab343724d8c814199724af61bf0b
Igor Alexandre <igoralexandrevieira652@gmail.com>
2025-12-28 13:01:27 -0400

    adicionando validacao na migracao de categorias

--- db/migrate/20251126000000_create_categoria.rb
+++ db/migrate/20251126000000_create_categoria.rb
@@ -1,13 +1,18 @@
 class CreateCategoria < ActiveRecord::Migration[7.2]
-  def change
+  def up
+    return if table_exists?(:categorias)
+
     create_table :categorias do |t|
       t.string :nome, null: false
       t.string :sexo, null: false
       t.integer :idade, null: false
       t.timestamps
     end
-    add_index :categorias, :nome
-    add_index :categorias, :sexo
-    add_index :categorias, :idade
+
+    add_index :categorias, [:nome, :sexo, :idade], unique: true
+  end
+
+  def down
+    drop_table :categorias if table_exists?(:categorias)
   end
 end
